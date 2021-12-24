c7ea56481361ac84bd7e20130e860ef7e0cf7fc6
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-12-24 11:22:15 -0600

    Created Snowing Component and added styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 //Components
 import SceneBase from './components/SceneBase';
+import SnowBG from './components/SnowBG';
 
 function App() {
   return (
     <div className="App flex items-center justify-center">
+      <SnowBG />
       <SceneBase />
     </div>
   );
---
+++ src/components/SnowBG.js
@@ -0,0 +1,34 @@
+import "../styles/snow-effect.css";
+
+function SnowBG() {
+  return (
+    <div class="snowflakes" aria-hidden="true">
+      <div class="snowflake">❅</div>
+      <div class="snowflake">❅</div>
+      <div class="snowflake">❆</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❅</div>
+      <div class="snowflake">❆</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❅</div>
+      <div class="snowflake">❆</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❅</div>
+      <div class="snowflake">❅</div>
+      <div class="snowflake">❆</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❅</div>
+      <div class="snowflake">❆</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❅</div>
+      <div class="snowflake">❆</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❄</div>
+      <div class="snowflake">❄</div>
+    </div>
+  );
+}
+
+export default SnowBG;
