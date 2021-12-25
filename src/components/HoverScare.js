e4461665e1426aa6ce815a4306f750ea09de853c
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-12-24 22:45:33 -0600

    Added Sound to the scare hover

--- src/components/HoverScare.js
+++ src/components/HoverScare.js
@@ -1,8 +1,28 @@
-function HoverScare() {
-  return(
-    <div className="hover-scare">
-  </div>
+import React, { useState } from "react";
+import useSound from "use-sound";
+import phasScare from "../audio/phas-scare.mp3";
+
+const HoverScare = () => {
+  //const [playScare] = useSound(phasScare, {volume: 0.25});
+
+  const [play, { stop }] = useSound(phasScare, { volume: 1 });
+
+  const [isHovering, setIsHovering] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => {
+        setIsHovering(true);
+        play();
+      }}
+      onMouseLeave={() => {
+        setIsHovering(false);
+        stop();
+      }}
+      isHovering={isHovering}
+      className="hover-scare"
+    ></div>
   );
-}
+};
 
 export default HoverScare;
